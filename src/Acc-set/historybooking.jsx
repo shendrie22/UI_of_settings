447065bc5e7973fddc593d477f8d3256ec5d94c7
shendrie22 <sherlinedeguzman3@gmail.com>
2024-05-30 17:44:54 +0800

    history booking

--- src/Acc-set/historybooking.jsx
+++ src/Acc-set/historybooking.jsx
@@ -10,7 +10,7 @@ const Historybooking = () => {
     switch (activeTab) {
       case 'today':
         return (
-          <div className="p-4 bg-white h-96 shadow-md rounded-b">
+          <div className="border rounded p-4 w-[110%] h-[500px]">
             <h2 className="text-lg font-bold mb-4">Booked desk today</h2>
             <table className="w-full text-left">
               <thead>
@@ -18,7 +18,7 @@ const Historybooking = () => {
                   <th className="border-b py-2 text-left w-1/4">Workspace</th>
                   <th className="border-b py-2 text-left w-1/4">Desk</th>
                   <th className="border-b py-2 text-left w-1/4">Date</th>
-                  <th className="border-b py-2 text-left w-1/4">Desk Map</th>
+                  <th className="border-b py-2 text-left w-1/4"></th>
                 </tr>
               </thead>
               <tbody>
@@ -29,7 +29,7 @@ const Historybooking = () => {
                   <td className="border-b py-2">
                     <div className="flex justify-between items-center">
                       <button 
-                        className="flex items-center text-blue-500 py-1 px-2 rounded"
+                        className="flex items-center text-black font-semibold py-1 px-2 rounded"
                         onClick={() => {
                           setImageUrl('/path/to/desk-map-today.png');
                           setShowImage(true);
@@ -37,7 +37,6 @@ const Historybooking = () => {
                       >
                         <FaMapMarkerAlt className="mr-2" /> View desk map
                       </button>
-                      {/* <button className="text-red-500 text-sm py-1 px-2 rounded"><FaTimes /></button> */}
                     </div>
                   </td>
                 </tr>
@@ -47,7 +46,7 @@ const Historybooking = () => {
         );
       case 'upcoming':
         return (
-          <div className="p-4 bg-white h-96 shadow-md rounded-b">
+          <div className="border rounded p-4 w-[110%] h-[500px]">
             <h2 className="text-lg font-bold mb-4">Upcoming reservations</h2>
             <table className="w-full text-left">
               <thead>
@@ -55,7 +54,7 @@ const Historybooking = () => {
                   <th className="border-b py-2 text-left w-1/4">Workspace</th>
                   <th className="border-b py-2 text-left w-1/4">Desk</th>
                   <th className="border-b py-2 text-left w-1/4">Date</th>
-                  <th className="border-b py-2 text-left w-1/4">Desk Map</th>
+                  <th className="border-b py-2 text-left w-1/4"></th>
                 </tr>
               </thead>
               <tbody>
@@ -66,7 +65,7 @@ const Historybooking = () => {
                   <td className="border-b py-2">
                     <div className="flex justify-between items-center">
                       <button 
-                        className="flex items-center text-blue-500 py-1 px-2 rounded"
+                        className="flex items-center text-black font-semibold py-1 px-2 rounded"
                         onClick={() => {
                           setImageUrl('/path/to/desk-map-upcoming1.png');
                           setShowImage(true);
@@ -74,7 +73,8 @@ const Historybooking = () => {
                       >
                         <FaMapMarkerAlt className="mr-2" /> View desk map
                       </button>
-                      <button className="text-red-500 text-sm py-1 px-2 rounded"><FaTimes /></button>
+                      <button className="text-white text-sm py-0.5 px-0.5 rounded bg-red-500 mr-4"><FaTimes />
+                   </button>
                     </div>
                   </td>
                 </tr>
@@ -85,7 +85,7 @@ const Historybooking = () => {
                   <td className="border-b py-2">
                     <div className="flex justify-between items-center">
                       <button 
-                        className="flex items-center text-blue-500 py-1 px-2 rounded"
+                        className="flex items-center text-black font-semibold py-1 px-2 rounded"
                         onClick={() => {
                           setImageUrl('/path/to/desk-map-upcoming2.png');
                           setShowImage(true);
@@ -93,7 +93,8 @@ const Historybooking = () => {
                       >
                         <FaMapMarkerAlt className="mr-2" /> View desk map
                       </button>
-                      <button className="text-red-500 text-sm py-1 px-2 rounded"><FaTimes /></button>
+                      <button className="text-white text-sm py-0.5 px-0.5 rounded bg-red-500 mr-4"><FaTimes />
+                   </button>
                     </div>
                   </td>
                 </tr>
@@ -103,7 +104,7 @@ const Historybooking = () => {
         );
       case 'pending':
         return (
-          <div className="p-4 bg-white h-96 shadow-md rounded-b">
+          <div className="border rounded p-4 w-[110%] h-[500px]">
             <h2 className="text-lg font-bold mb-4">Pending reservations</h2>
             <table className="w-full text-left">
               <thead>
@@ -111,18 +112,18 @@ const Historybooking = () => {
                   <th className="border-b py-2 text-left w-1/4">Workspace</th>
                   <th className="border-b py-2 text-left w-1/4">Desk</th>
                   <th className="border-b py-2 text-left w-1/4">Date</th>
-                  <th className="border-b py-2 text-left w-1/4">Desk Map</th>
+                  <th className="border-b py-2 text-left w-1/4"></th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
-                  <td className="border-b py-2">Office Area 2</td>
-                  <td className="border-b py-2">Desk A3</td>
-                  <td className="border-b py-2">June 01, 2024</td>
+                  <td className="border-b py-2">Office Area C</td>
+                  <td className="border-b py-2">Desk C1</td>
+                  <td className="border-b py-2">June 05, 2024</td>
                   <td className="border-b py-2">
                     <div className="flex justify-between items-center">
                       <button 
-                        className="flex items-center text-blue-500 py-1 px-2 rounded"
+                        className="flex items-center text-black font-semibold py-1 px-2 rounded"
                         onClick={() => {
                           setImageUrl('/path/to/desk-map-pending1.png');
                           setShowImage(true);
@@ -130,7 +131,8 @@ const Historybooking = () => {
                       >
                         <FaMapMarkerAlt className="mr-2" /> View desk map
                       </button>
-                      <button className="text-red-500 text-sm py-1 px-2 rounded"><FaTimes /></button>
+                      <button className="text-white text-sm py-0.5 px-0.5 rounded bg-red-500 mr-4"><FaTimes />
+                   </button>
                     </div>
                   </td>
                 </tr>
@@ -141,7 +143,7 @@ const Historybooking = () => {
                   <td className="border-b py-2">
                     <div className="flex justify-between items-center">
                       <button 
-                        className="flex items-center text-blue-500 py-1 px-2 rounded"
+                        className="flex items-center text-black font-semibold py-1 px-2 rounded"
                         onClick={() => {
                           setImageUrl('/path/to/desk-map-pending2.png');
                           setShowImage(true);
@@ -149,7 +151,9 @@ const Historybooking = () => {
                       >
                         <FaMapMarkerAlt className="mr-2" /> View desk map
                       </button>
-                      <button className="text-red-500 text-sm py-1 px-2 rounded"><FaTimes /></button>
+                      <button className="text-white text-sm py-0.5 px-0.5 rounded bg-red-500 mr-4"><FaTimes />
+                   </button>
+  
                     </div>
                   </td>
                 </tr>
@@ -159,68 +163,63 @@ const Historybooking = () => {
         );
       case 'cancelled':
         return (
-          <div className="p-4 bg-white h-96 shadow-md rounded-b">
+          <div className="border rounded p-4 w-[110%] h-[500px]">
             <h2 className="text-lg font-bold mb-4">Cancelled reservations</h2>
             <table className="w-full text-left">
               <thead>
                 <tr>
-                  <th className="border-b py-2 text-left w-1/3">Workspace</th>
-                  <th className="border-b py-2 text-left w-1/3">Desk</th>
-                  <th className="border-b py-2 text-left w-1/3">Date</th>
+                  <th className="border-b py-2 text-left w-1/4">Workspace</th>
+                  <th className="border-b py-2 text-left w-1/4">Desk</th>
+                  <th className="border-b py-2 text-left w-1/4">Date</th>
+                  <th className="border-b py-2 text-left w-1/4"></th>
                 </tr>
               </thead>
-              <tbody>
-                <tr>
-                  <td className="border-b py-2">Office Area 1</td>
-                  <td className="border-b py-2">Desk B2</td>
-                  <td className="border-b py-2">May 25, 2024</td>
-                </tr>
-                <tr>
-                  <td className="border-b py-2">Office Area 3</td>
-                  <td className="border-b py-2">Desk C4</td>
-                  <td className="border-b py-2">May 20, 2024</td>
-                </tr>
-              </tbody>
             </table>
           </div>
         );
       case 'history':
         return (
-          <div className="p-4 bg-white h-96 shadow-md rounded-b">
-            <h2 className="text-lg font-bold mb-4">Booking history</h2>
+          <div className="border rounded p-4 w-[110%] h-[500px]">
+            <h2 className="text-lg font-bold mb-4">History</h2>
             <table className="w-full text-left">
               <thead>
                 <tr>
-                  <th className="border-b py-2 text-left w-1/3">Workspace</th>
-                  <th className="border-b py-2 text-left w-1/3">Desk</th>
-                  <th className="border-b py-2 text-left w-1/3">Date</th>
+                  <th className="border-b py-2 text-left w-1/4">Workspace</th>
+                  <th className="border-b py-2 text-left w-1/4">Desk</th>
+                  <th className="border-b py-2 text-left w-1/4">Date</th>
+                  <th className="border-b py-2 text-left w-1/4"></th>
                 </tr>
               </thead>
               <tbody>
-                <tr>
+                <tr className="h-12">
                   <td className="border-b py-2">Office Area C</td>
                   <td className="border-b py-2">Desk C1</td>
                   <td className="border-b py-2">May 23, 2024</td>
+                  <td className="border-b py-2"></td>
                 </tr>
-                <tr>
+                <tr className="h-12">
                   <td className="border-b py-2">Office Area 2</td>
                   <td className="border-b py-2">Desk A3</td>
                   <td className="border-b py-2">May 15, 2024</td>
+                  <td className="border-b py-2"></td>
                 </tr>
-                <tr>
+                <tr className="h-12">
                   <td className="border-b py-2">Office Area 2</td>
                   <td className="border-b py-2">Desk A3</td>
                   <td className="border-b py-2">May 14, 2024</td>
+                  <td className="border-b py-2"></td>
                 </tr>
-                <tr>
+                <tr className="h-12">
                   <td className="border-b py-2">Office Area 2</td>
                   <td className="border-b py-2">Desk A3</td>
                   <td className="border-b py-2">April 30, 2024</td>
+                  <td className="border-b py-2"></td>
                 </tr>
-                <tr>
+                <tr className="h-12">
                   <td className="border-b py-2">Office Area 2</td>
                   <td className="border-b py-2">Desk A3</td>
                   <td className="border-b py-2">April 13, 2024</td>
+                  <td className="border-b py-2"></td>
                 </tr>
               </tbody>
             </table>
@@ -236,31 +235,31 @@ const Historybooking = () => {
       <h1 className="text-2xl font-bold mb-6">My Bookings</h1>
       <div className="flex mb-0 border-b">
         <button
-          className={`flex-1 py-2 px-4 text-center ${activeTab === 'today' ? 'bg-black text-white' : 'bg-gray-100 text-black'}`}
+          className={`flex-1 py-2 px-4 text-center mr-2 border rounded ${activeTab === 'today' ? 'text-black' : 'text-black'} hover:bg-black hover:text-white`}
           onClick={() => setActiveTab('today')}
         >
           <FaCalendarCheck className="inline mr-2" /> Today
         </button>
         <button
-          className={`flex-1 py-2 px-4 text-center ${activeTab === 'upcoming' ? 'bg-black text-white' : 'bg-gray-100 text-black'}`}
+          className={`flex-1 py-2 px-4 text-center mr-2 border rounded ${activeTab === 'upcoming' ? 'text-black' : 'text-black'} hover:bg-black hover:text-white`}
           onClick={() => setActiveTab('upcoming')}
         >
           <FaRegCalendarAlt className="inline mr-2" /> Upcoming
         </button>
         <button
-          className={`flex-1 py-2 px-4 text-center ${activeTab === 'pending' ? 'bg-black text-white' : 'bg-gray-100 text-black'}`}
+          className={`flex-1 py-2 px-4 text-center mr-2 border rounded ${activeTab === 'pending' ? 'text-black' : 'text-black'} hover:bg-black hover:text-white`}
           onClick={() => setActiveTab('pending')}
         >
           <FaClock className="inline mr-2" /> Pending
         </button>
         <button
-          className={`flex-1 py-2 px-4 text-center ${activeTab === 'cancelled' ? 'bg-black text-white' : 'bg-gray-100 text-black'}`}
+          className={`flex-1 py-2 px-4 text-center mr-2 border rounded ${activeTab === 'cancelled' ? 'text-black' : 'text-black'} hover:bg-black hover:text-white`}
           onClick={() => setActiveTab('cancelled')}
         >
           <FaBan className="inline mr-2" /> Cancelled
         </button>
         <button
-          className={`flex-1 py-2 px-4 text-center ${activeTab === 'history' ? 'bg-black text-white' : 'bg-gray-100 text-black'}`}
+          className={`flex-1 py-2 px-4 text-center border rounded ${activeTab === 'history' ? 'text-black' : 'text-black'} hover:bg-black hover:text-white`}
           onClick={() => setActiveTab('history')}
         >
           <FaHistory className="inline mr-2" /> History
